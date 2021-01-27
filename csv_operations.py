d80a47f76a768591cdcffe1addb4943deff35ac6
nattri7 <77777307+nattri7@users.noreply.github.com>
2021-01-27 14:21:36 +0530

    script.py

---
+++ csv_operations.py
@@ -0,0 +1,6 @@
+#########################################################################################
+# write a script to create a new csv file from DATA.csv file
+# Script should take DATA.csv as arugment
+# in new csv file should have 3 columns "Host Name", "Node IP", "Manufacturer" 
+# cisco manufacturer node IP should have /24 subnet and brocade should have /32
+#########################################################################################
